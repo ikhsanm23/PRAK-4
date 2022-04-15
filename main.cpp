476ee7161c2f70b34b16838a6bcf522b27a4abd2
Ikhsan Maulana <101860073+ikhsanm23@users.noreply.github.com>
2022-04-15 23:30:50 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include "library/input.h"
+#include "library/proses.h"
+#include "library/output.h"
+using namespace std;
+
+int main() {
+  Input input;
+  input.cetak();
+  input.toFile();
+
+  Proses proses;
+  proses.getData();
+  proses.proses();
+  proses.toFile();
+
+  Output output;
+  output.getData();
+  output.dataOut();
+  
+  return 0;
+}
